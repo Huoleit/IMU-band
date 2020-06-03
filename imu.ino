1b5bf001ec0347dc28a4dcda04cd01749dae2593
Cecil <zfuaj@connect.ust.hk>
2020-06-03 21:21:02 +0800

    dual imu finished! But system will hang due to I2C a few minutes after starting

--- imu.ino
+++ imu.ino
@@ -32,7 +32,7 @@ float   SelfTest[6];    // holds results of gyro and accelerometer self test
 int32_t   gyroBias1[3] = {414, -265, 20};
 float accelBias1[3] = {-1.66613769f, -2.16113281f, -0.33935546f};
 
-int32_t   gyroBias2[3] = {0, 0, 0};
+int32_t   gyroBias2[3] = {388, 160, -2};
 float accelBias2[3] = {0.0f, 0.0f, 0.0f};
 
 float   magBias1[3] = {415.33, -209.91, -229.41}, magScale1[3]  = {1.20, 0.91, 0.93}; // Bias corrections for gyro and accelerometer
@@ -49,206 +49,250 @@ uint32_t Now1 = 0, Now2 = 0;                         // used to calculate integr
 
 float ax1, ay1, az1, gx1, gy1, gz1, mx1, my1, mz1; // variables to hold latest sensor data values 
 float ax2, ay2, az2, gx2, gy2, gz2, mx2, my2, mz2; // variables to hold latest sensor data values 
-Quaternion orien(1.0f,0.,0.,0.);
+Quaternion orien_1(1.0f,0.,0.,0.);
+Quaternion orien_2(1.0f,0.,0.,0.);
 
 MPU9250 MPU9250(intPin1); // instantiate MPU9250 class
-uint32_t last_send_time1 = 0; 
-uint32_t delta_send_time1 = 0; 
+uint32_t last_send_time = 0; 
+uint32_t delta_send_time = 0; 
+bool isOnline_1 = false;
+bool isOnline_2 = false;
 
 void setup()
 {
   Serial.begin(115200);
-  delay(1000);
+  delay(200);
   
-  Wire.begin(); // set master mode, default on SDA/SCL for Ladybug   
-  Wire.setClock(400000); // I2C frequency at 400 kHz
-  delay(1000);
+  Wire.begin(); 
+  Wire.setClock(400000); 
+  delay(200);
 
-  MPU9250.I2Cscan(); // should detect BME280 at 0x77, MPU9250 at 0x71 
+  // MPU9250.I2Cscan();
   
-  // Set up the interrupt pin, it's set as active high, push-pull
   pinMode(myLed, OUTPUT);
-  digitalWrite(myLed, HIGH); // start with orange led on (active HIGH)
+  digitalWrite(myLed, HIGH); 
 
-  pinMode(intPin1, INPUT);
+  pinMode(intPin1, INPUT); // interrupt
   pinMode(intPin2, INPUT);
 
-   /* Configure the MPU9250 */
-  // Read the WHO_AM_I register, this is a good test of communication
   Serial.println("MPU9250 9-axis motion sensor...");
   uint8_t c = MPU9250.getMPU9250ID(MPU1);
-  Serial.print("MPU9250_1 "); Serial.print("I AM "); Serial.print(c, HEX); Serial.print(" I should be "); Serial.println(0x71, HEX);
   uint8_t d = MPU9250.getMPU9250ID(MPU2);
-  Serial.print("MPU9250_2 "); Serial.print("I AM "); Serial.print(d, HEX); Serial.print(" I should be "); Serial.println(0x71, HEX);
-  delay(1000);
-  
-  // if (c == 0x71 && d == 0x71 ) // WHO_AM_I should always be 0x71 for MPU9250, 0x73 for MPU9255 
   if (c == 0x71)
-  {  
-    Serial.println("MPU9250s 1 is online...");
-    
-    delay(1000);
-
+  {
+    Serial.println("MPU 1 is online");
+    isOnline_1 = true;
+  }
+  if (d == 0x71)
+  {
+    Serial.println("MPU 2 is online");
+    isOnline_2 = true;
+  }
+  if(!isOnline_1 && !isOnline_2)
+  {
+    Serial.print("Could not connect to MPU9250"); Serial.println(c, HEX);
+    while(1) ; 
+  }
   // get sensor resolutions, only need to do this once, same for both MPU9250s for now
   aRes = MPU9250.getAres(Ascale);
   gRes = MPU9250.getGres(Gscale);
   mRes = MPU9250.getMres(Mscale);
 
 #if CALIBRATION_GYRO
-    MPU9250.calibrateMPU9250(MPU1, gyroBias1, NULL); // Calibrate gyro and accelerometers, load biases in bias registers
-    Serial.println("MPU1 accel biases (g)"); Serial.println(accelBias1[0],10); Serial.println(accelBias1[1],8); Serial.println(accelBias1[2],8);
-    Serial.println("MPU1 gyro biases (dps)"); Serial.println(gyroBias1[0]); Serial.println(gyroBias1[1]); Serial.println(gyroBias1[2]);
+    MPU9250.calibrateMPU9250(MPU1, gyroBias1, accelBias1); // Calibrate gyro and accelerometers, load biases in bias registers
+    Serial.println("MPU accel biases (g)"); Serial.println(accelBias1[0],10); Serial.println(accelBias1[1],10); Serial.println(accelBias1[2],10);
+    Serial.println("MPU gyro biases"); Serial.println(gyroBias1[0]); Serial.println(gyroBias1[1]); Serial.println(gyroBias1[2]);
     Serial.println("Finished!! Please reset");
     while(1);
 #endif
-  MPU9250.GyroBiasWriteToReg(MPU1, gyroBias1);
-  // MPU9250.calibrateMPU9250(MPU2, gyroBias2, accelBias2); // Calibrate gyro and accelerometers, load biases in bias registers
-  // Serial.println("MPU2 accel biases (mg)"); Serial.println(1000.*accelBias2[0]); Serial.println(1000.*accelBias2[1]); Serial.println(1000.*accelBias2[2]);
-  // Serial.println("MPU2 gyro biases (dps)"); Serial.println(gyroBias2[0]); Serial.println(gyroBias2[1]); Serial.println(gyroBias2[2]);
-  // delay(1000); 
-  
-  MPU9250.initMPU9250(MPU1, Ascale, Gscale, sampleRate); 
-  // MPU9250.initMPU9250(MPU2, Ascale, Gscale, sampleRate); 
-  Serial.println("MPU9250s 1 initialized for active data mode...."); // Initialize device for active mode read of acclerometer, gyroscope, and temperature
-  
-  // Read the WHO_AM_I register of the magnetometer, this is a good test of communication
-  byte e = MPU9250.getAK8963CID(MPU1);  // Read WHO_AM_I register for AK8963
-  Serial.print("AK8963 1 "); Serial.print("I AM "); Serial.print(e, HEX); Serial.print(" I should be "); Serial.println(0x48, HEX);
-  // byte f = MPU9250.getAK8963CID(MPU2);  // Read WHO_AM_I register for AK8963
-  // Serial.print("AK8963 2 "); Serial.print("I AM "); Serial.print(f, HEX); Serial.print(" I should be "); Serial.println(0x48, HEX);
-  delay(1000); 
-  
-  // Get magnetometer calibration from AK8963 ROM
-  MPU9250.initAK8963Slave(MPU1, Mscale, Mmode, magCalibration1); Serial.println("AK8963 1 initialized for active data mode...."); // Initialize device 1 for active mode read of magnetometer
-  Serial.println("Calibration values for mag 1: ");
-  Serial.print("X-Axis sensitivity adjustment value "); Serial.println(magCalibration1[0], 2);
-  Serial.print("Y-Axis sensitivity adjustment value "); Serial.println(magCalibration1[1], 2);
-  Serial.print("Z-Axis sensitivity adjustment value "); Serial.println(magCalibration1[2], 2);
-  // MPU9250.initAK8963Slave(MPU2, Mscale, Mmode, magCalibration2); Serial.println("AK8963 2 initialized for active data mode...."); // Initialize device 2 for active mode read of magnetometer
-  // Serial.println("Calibration values for mag 2: ");
-  // Serial.print("X-Axis sensitivity adjustment value "); Serial.println(magCalibration2[0], 2);
-  // Serial.print("Y-Axis sensitivity adjustment value "); Serial.println(magCalibration2[1], 2);
-  // Serial.print("Z-Axis sensitivity adjustment value "); Serial.println(magCalibration2[2], 2);
-  
- // Comment out if using pre-measured, pre-stored offset biases
+//update gyro bias reg
+  if(isOnline_1)
+  {
+    MPU9250.GyroBiasWriteToReg(MPU1, gyroBias1);
+    delay(200);
+    MPU9250.initMPU9250(MPU1, Ascale, Gscale, sampleRate); 
+    byte e = MPU9250.getAK8963CID(MPU1);  // Read WHO_AM_I register for AK8963
+    Serial.print("AK8963 1 "); Serial.print("I AM "); Serial.print(e, HEX); Serial.print(" I should be "); Serial.println(0x48, HEX);
+    MPU9250.initAK8963Slave(MPU1, Mscale, Mmode, magCalibration1); Serial.println("AK8963 1 initialized for active data mode...."); // Initialize device 1 for active mode read of magnetometer
+  }
+  if(isOnline_2)
+  {
+    MPU9250.GyroBiasWriteToReg(MPU2, gyroBias2);
+    delay(200);
+    MPU9250.initMPU9250(MPU2, Ascale, Gscale, sampleRate); 
+    byte e = MPU9250.getAK8963CID(MPU2);  // Read WHO_AM_I register for AK8963
+    Serial.print("AK8963 2 "); Serial.print("I AM "); Serial.print(e, HEX); Serial.print(" I should be "); Serial.println(0x48, HEX);
+    MPU9250.initAK8963Slave(MPU2, Mscale, Mmode, magCalibration1); Serial.println("AK8963 2 initialized for active data mode...."); // Initialize device 2 for active mode read of magnetometer
+  }
+    
 #if CALIBRATION_MAG
   MPU9250.magcalMPU9250(MPU1, magBias1, magScale1);
   Serial.println("AK8963 1 mag biases (mG)"); Serial.println(magBias1[0]); Serial.println(magBias1[1]); Serial.println(magBias1[2]); 
   Serial.println("AK8963 1 mag scale (mG)"); Serial.println(magScale1[0]); Serial.println(magScale1[1]); Serial.println(magScale1[2]); 
   while(1);
 #endif
-  // MPU9250.magcalMPU9250(MPU2, magBias2, magScale2);
-  // Serial.println("AK8963 2 mag biases (mG)"); Serial.println(magBias2[0]); Serial.println(magBias2[1]); Serial.println(magBias2[2]); 
-  // Serial.println("AK8963 2 mag scale (mG)"); Serial.println(magScale2[0]); Serial.println(magScale2[1]); Serial.println(magScale2[2]); 
-  // delay(2000); // add delay to see results before serial spew of data
- 
   
   attachInterrupt(digitalPinToInterrupt(intPin1), myinthandler1, RISING);  // define interrupt for intPin output of MPU9250 1
   attachInterrupt(digitalPinToInterrupt(intPin2), myinthandler2, RISING);  // define interrupt for intPin output of MPU9250 2
 
-  
-  }
-  else
-  {
-    Serial.print("Could not connect to MPU9250 1: 0x"); Serial.println(c, HEX);
-    Serial.print("Could not connect to MPU9250 2: 0x"); Serial.println(d, HEX);
-    while(1) ; // Loop forever if communication doesn't happen
-  }
-
-
-   digitalWrite(myLed, LOW); // turn off led when using flash memory
-
-
-  
+  digitalWrite(myLed, LOW); // turn off led when using flash memory
 }
 
 void loop()
 {
-  // digitalWrite(myLed, HIGH);
-   if(intFlag1 == true) {   
+   if(intFlag1 == true && isOnline_1) {   
       intFlag1 = false; 
 
     MPU9250.readMPU9250Data(MPU1, MPU9250Data1); // INT cleared on any read
    
-    // Now we'll calculate the accleration value into actual g's
-     ax1 = (float)MPU9250Data1[0]*aRes - accelBias1[0];  // get actual g value, this depends on scale being set
-     ay1 = (float)MPU9250Data1[1]*aRes - accelBias1[1];   
-     az1 = (float)MPU9250Data1[2]*aRes - accelBias1[2];  
-
-    // Calculate the gyro value into actual degrees per second
-     gx1 = (float)MPU9250Data1[4]*gRes*deg2rad;  // get actual gyro value, this depends on scale being set
-     gy1 = (float)MPU9250Data1[5]*gRes*deg2rad;  
-     gz1 = (float)MPU9250Data1[6]*gRes*deg2rad; 
-  
-//    if( MPU9250.checkNewMagData() == true) { // wait for magnetometer data ready bit to be set
-      MPU9250.readMagData(MPU1, magCount1);  // Read the x/y/z adc values
-  
-    // Calculate the magnetometer values in milliGauss
-    // Include factory calibration per data sheet and user environmental corrections
-      mx1 = (float)magCount1[0]*mRes*magCalibration1[0] - magBias1[0];  // get actual magnetometer value, this depends on scale being set
-      my1 = (float)magCount1[1]*mRes*magCalibration1[1] - magBias1[1];  
-      mz1 = (float)magCount1[2]*mRes*magCalibration1[2] - magBias1[2];  
-      mx1 *= magScale1[0];
-      my1 *= magScale1[1];
-      mz1 *= magScale1[2]; 
-//    }
+    ax1 = (float)MPU9250Data1[0]*aRes - accelBias1[0];  
+    ay1 = (float)MPU9250Data1[1]*aRes - accelBias1[1];   
+    az1 = (float)MPU9250Data1[2]*aRes - accelBias1[2];  
+    gx1 = (float)MPU9250Data1[4]*gRes*deg2rad; 
+    gy1 = (float)MPU9250Data1[5]*gRes*deg2rad;  
+    gz1 = (float)MPU9250Data1[6]*gRes*deg2rad; 
+    
+    // MPU9250.readMagData(MPU1, magCount1); 
+    // mx1 = (float)magCount1[0]*mRes*magCalibration1[0] - magBias1[0];  
+    // my1 = (float)magCount1[1]*mRes*magCalibration1[1] - magBias1[1];  
+    // mz1 = (float)magCount1[2]*mRes*magCalibration1[2] - magBias1[2];  
+    // mx1 *= magScale1[0];
+    // my1 *= magScale1[1];
+    // mz1 *= magScale1[2]; 
    
     Now1 = micros();
     deltat1 = ((Now1 - lastUpdate1)/1000000.0f); // set integration time by time elapsed since last filter update
     lastUpdate1 = Now1;
 
     Quaternion omega(0, gx1,gy1,gz1);
-    Quaternion delta_q = orien * omega * 0.5f;
-    orien = orien + (delta_q * deltat1);
-    orien.normalize();
-
-    orien.toEulerAngle(&roll1, &pitch1,&yaw1);
-    roll1 *= rad2deg;
-    pitch1 *= rad2deg;
-    yaw1 *= rad2deg;
+    Quaternion delta_q = orien_1 * omega * 0.5f;
+    orien_1 = orien_1 + (delta_q * deltat1);
+    orien_1.normalize();
    
     sum1 += deltat1; // sum for averaging filter update rate
     sumCount1++;
-    if(sum1 >= 1.0f) //1HZ 
-    {
-      if (SERIAL_DEBUG)
-      {
-        Serial.print("rate 1 = "); Serial.print((float)sumCount1/sum1, 2); Serial.println(" Hz");
-        Serial.print("ax1 = "); Serial.print((int)1000*ax1);  
-        Serial.print(" ay1 = "); Serial.print((int)1000*ay1); 
-        Serial.print(" az1 = "); Serial.print((int)1000*az1); Serial.println(" mg");
-        Serial.print("gx1 = "); Serial.print( gx1, 4); 
-        Serial.print(" gy1 = "); Serial.print( gy1, 4); 
-        Serial.print(" gz1 = "); Serial.print( gz1, 4); Serial.println(" deg/s");
-        Serial.print("mx1 = "); Serial.print( (int)mx1 ); 
-        Serial.print(" my1 = "); Serial.print( (int)my1 ); 
-        Serial.print(" mz1 = "); Serial.print( (int)mz1 ); Serial.println(" mG");
-        Serial.print("MPU9250 1 Yaw, Pitch, Roll: ");
-        Serial.print(yaw1, 2);
-        Serial.print(", ");
-        Serial.print(pitch1, 2);
-        Serial.print(", ");
-        Serial.println(roll1, 2);
-      }
-      sum1 = 0;
-      sumCount1 = 0;
-    }
-
+   
     /* end of MPU9250 1 interrupt handling */
    }
 
-   delta_send_time1 = millis() - last_send_time1;
-   if (!SERIAL_DEBUG && delta_send_time1 >= 10) //100HZ
+   if(intFlag2 == true && isOnline_2) {   
+      intFlag2 = false; 
+    // Serial.print("M");
+    delayMicroseconds(10);
+    MPU9250.readMPU9250Data(MPU2, MPU9250Data2); // INT cleared on any read
+    //Serial.print("read");
+   
+    ax2 = (float)MPU9250Data2[0]*aRes - accelBias2[0];  
+    ay2 = (float)MPU9250Data2[1]*aRes - accelBias2[1];   
+    az2 = (float)MPU9250Data2[2]*aRes - accelBias2[2];  
+    gx2 = (float)MPU9250Data2[4]*gRes*deg2rad; 
+    gy2 = (float)MPU9250Data2[5]*gRes*deg2rad;  
+    gz2 = (float)MPU9250Data2[6]*gRes*deg2rad; 
+    
+    // MPU9250.readMagData(MPU2, magCount2); 
+    // mx2 = (float)magCount2[0]*mRes*magCalibration2[0] - magBias2[0];  
+    // my2 = (float)magCount2[1]*mRes*magCalibration2[1] - magBias2[1];  
+    // mz2 = (float)magCount2[2]*mRes*magCalibration2[2] - magBias2[2];  
+    // mx2 *= magScale2[0];
+    // my2 *= magScale2[1];
+    // mz2 *= magScale2[2]; 
+   
+    Now2 = micros();
+    deltat2 = ((Now2 - lastUpdate2)/1000000.0f); // set integration time by time elapsed since last filter update
+    lastUpdate2 = Now2;
+
+    Quaternion omega(0, gx2,gy2,gz2);
+    Quaternion delta_q = orien_2 * omega * 0.5f;
+    orien_2 = orien_2 + (delta_q * deltat2);
+    orien_2.normalize();
+   
+    sum2 += deltat2; // sum for averaging filter update rate
+    sumCount2++;
+    // Serial.println("AAAAAAA");
+    /* end of MPU9250 2 interrupt handling */
+   }
+
+    if(SERIAL_DEBUG && isOnline_1 && sum1 >= 1.0f) //1HZ 
+    {
+      orien_1.toEulerAngle(&roll1, &pitch1,&yaw1);
+      roll1 *= rad2deg;
+      pitch1 *= rad2deg;
+      yaw1 *= rad2deg;
+
+      Serial.print("rate 1 = "); Serial.print((float)sumCount1/sum1, 2); Serial.println(" Hz");
+      Serial.print("ax1 = "); Serial.print((int)1000*ax1);  
+      Serial.print(" ay1 = "); Serial.print((int)1000*ay1); 
+      Serial.print(" az1 = "); Serial.print((int)1000*az1); Serial.println(" mg");
+      Serial.print("gx1 = "); Serial.print( gx1, 4); 
+      Serial.print(" gy1 = "); Serial.print( gy1, 4); 
+      Serial.print(" gz1 = "); Serial.print( gz1, 4); Serial.println(" deg/s");
+      Serial.print("mx1 = "); Serial.print( (int)mx1 ); 
+      Serial.print(" my1 = "); Serial.print( (int)my1 ); 
+      Serial.print(" mz1 = "); Serial.print( (int)mz1 ); Serial.println(" mG");
+      Serial.print("MPU9250 1 Yaw, Pitch, Roll: ");
+      Serial.print(yaw1, 2);
+      Serial.print(", ");
+      Serial.print(pitch1, 2);
+      Serial.print(", ");
+      Serial.println(roll1, 2);
+
+      sum1 = 0;
+      sumCount1 = 0;
+    }
+  
+  if(SERIAL_DEBUG && isOnline_2 && sum2 >= 1.0f) //1HZ 
+    {
+      orien_2.toEulerAngle(&roll2, &pitch2,&yaw2);
+      roll2 *= rad2deg;
+      pitch2 *= rad2deg;
+      yaw2 *= rad2deg;
+
+      Serial.print("rate 2 = "); Serial.print((float)sumCount2/sum2, 2); Serial.println(" Hz");
+      Serial.print("ax2 = "); Serial.print((int)1000*ax2);  
+      Serial.print(" ay2 = "); Serial.print((int)1000*ay2); 
+      Serial.print(" az2 = "); Serial.print((int)1000*az2); Serial.println(" mg");
+      Serial.print("gx2 = "); Serial.print( gx2, 4); 
+      Serial.print(" gy2 = "); Serial.print( gy2, 4); 
+      Serial.print(" gz2 = "); Serial.print( gz2, 4); Serial.println(" deg/s");
+      Serial.print("mx2 = "); Serial.print( (int)mx2 ); 
+      Serial.print(" my2 = "); Serial.print( (int)my2 ); 
+      Serial.print(" mz2 = "); Serial.print( (int)mz2 ); Serial.println(" mG");
+      Serial.print("MPU9250 2 Yaw, Pitch, Roll: ");
+      Serial.print(yaw2, 2);
+      Serial.print(", ");
+      Serial.print(pitch2, 2);
+      Serial.print(", ");
+      Serial.println(roll2, 2);
+
+      sum2 = 0;
+      sumCount2 = 0;
+    }
+
+   delta_send_time = millis() - last_send_time;
+   if (!SERIAL_DEBUG && delta_send_time >= 10) //100HZ
    {
-     last_send_time1 = millis();
+     last_send_time = millis();
      Serial.print(String("#;"));
-     Serial.print(orien.w, 5);
+     Serial.print(orien_1.w, 5);
      Serial.print(String(';'));
-     Serial.print(orien.x, 5);
+     Serial.print(orien_1.x, 5);
      Serial.print(String(';'));
-     Serial.print(orien.y, 5);
+     Serial.print(orien_1.y, 5);
      Serial.print(String(';'));
-     Serial.print(orien.z, 5);
+     Serial.print(orien_1.z, 5);
+     Serial.print('\n');
+
+     
+     Serial.print(String("$;"));
+     Serial.print(orien_2.w, 5);
+     Serial.print(String(';'));
+     Serial.print(orien_2.x, 5);
+     Serial.print(String(';'));
+     Serial.print(orien_2.y, 5);
+     Serial.print(String(';'));
+     Serial.print(orien_2.z, 5);
+     Serial.print(String(';'));
+     Serial.print(delta_send_time);
      Serial.print('\n');
    }
 }
