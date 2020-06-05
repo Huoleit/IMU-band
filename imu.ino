e15398c76d96b7eb384ec1c429ad10189f8cf0f8
Cecil <zfuaj@connect.ust.hk>
2020-06-05 12:03:43 +0800

    update parameters

--- imu.ino
+++ imu.ino
@@ -29,10 +29,10 @@ float   temperature1, temperature2;    // Stores the MPU9250 internal chip tempe
 float   SelfTest[6];    // holds results of gyro and accelerometer self test
 
 // These can be measured once and entered here or can be calculated each time the device is powered on
-int32_t   gyroBias1[3] = {414, -265, 20};
+int32_t   gyroBias1[3] = {411, 184, 7};
 float accelBias1[3] = {-1.66613769f, -2.16113281f, -0.33935546f};
 
-int32_t   gyroBias2[3] = {388, 160, -2};
+int32_t   gyroBias2[3] = {-89, 75, 26};
 float accelBias2[3] = {0.0f, 0.0f, 0.0f};
 
 float   magBias1[3] = {415.33, -209.91, -229.41}, magScale1[3]  = {1.20, 0.91, 0.93}; // Bias corrections for gyro and accelerometer
